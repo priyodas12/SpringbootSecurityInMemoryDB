b0e5551ebe35b5f99eb9cab74a83118a560a6523
Priyobrato Das <priyodas12@gmail.com>
2020-12-29 15:00:54 +0530

    2nd commit

---
+++ src/main/java/io/springlab/SpringbootSecurityInMemoryDB/Controller/GenericController.java
@@ -0,0 +1,36 @@
+package io.springlab.SpringbootSecurityInMemoryDB.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class GenericController {
+
+    @RequestMapping(method = RequestMethod.GET ,value = "/home")
+    public String gotoHomePage(){
+        return "HomePage";
+    }
+
+    @RequestMapping(method = RequestMethod.GET ,value ="/welcome")
+    public String gotoWelcomePage(){
+        return "WelcomePage";
+    }
+
+    @RequestMapping(method = RequestMethod.GET ,value ="/admin")
+    public String gotoAdminPage(){
+        return "AdminPage";
+    }
+
+    @RequestMapping(method = RequestMethod.GET ,value ="/emp")
+    public String gotoEmpPage(){
+        return "EmpPage";
+    }
+
+    @RequestMapping(method = RequestMethod.GET ,value ="/clerk")
+    public String gotoClerkPage(){
+        return "ClerkPage";
+    }
+}
