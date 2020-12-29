9b8f595c672f4096a8b6d95f669d2fa8b34474f3
Priyobrato Das <priyodas12@gmail.com>
2020-12-29 14:26:49 +0530

    first commit

---
+++ src/main/java/io/springlab/SpringbootSecurityInMemoryDB/SpringbootSecurityInMemoryDbApplication.java
@@ -0,0 +1,13 @@
+package io.springlab.SpringbootSecurityInMemoryDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityInMemoryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityInMemoryDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/springlab/SpringbootSecurityInMemoryDB/SpringbootSecurityInMemoryDbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springlab.SpringbootSecurityInMemoryDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityInMemoryDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
